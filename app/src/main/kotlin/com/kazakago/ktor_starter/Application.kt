b0184c35230722e68937fe8e5f4b5f0f08fd032e
Kensuke Tamura <kazakago@gmail.com>
2021-01-05 13:34:21 +0900

    update ktor

--- app/src/main/kotlin/com/kazakago/ktor_starter/Application.kt
+++ app/src/main/kotlin/com/kazakago/ktor_starter/Application.kt
@@ -1,16 +1,13 @@
 package com.kazakago.ktor_starter
 
-import io.ktor.application.Application
-import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.response.respond
-import io.ktor.routing.Routing
-import io.ktor.routing.get
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun Application.module() {
     install(Routing) {
         get("/") {
-            call.respond("Hello world from Ktor!!")
+            call.respond("Hello world from Ktor!")
         }
     }
 }
